f20915102bafcfa179da73f36ec77070ef69fc54
LucasPiffer <piffer.lucas@gmail.com>
2018-10-14 15:24:00 -0300

    Create Dockerfile to run node application. In production change it to use npm2

---
+++ nodewithkubernetes/node-nominator/app/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000
+
+const router = express.Router();
+
+router.route('/Name')
+  .get((req, res) => {
+     const name = 'Lucas';
+     res.send("You should use " + name)
+   })
+
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log('Running on port ' + port);
+})
---
+++ nodewithkubernetes/node-nominator/app/gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp'),
+  nodemon = require('gulp-nodemon');
+
+gulp.task('start', () => {
+   nodemon({
+     script: "./app.js",
+     env: {
+       PORT: "8000"
+     }
+   })
+})
